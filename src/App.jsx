b740703b7a6e2d935cf61692f1d1ec855e3ec13d
bwk0143-commits <bwk0143@gmail.com>
2026-07-01 23:32:17 +0530

    app.jsx save

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import Navbar from "./components/navbar";
+import Sidebar from "./components/sidebar";
+
+import "./styles/navbar.css";
+import "./styles/sidebar.css";
+
 function App() {
   return (
-    <h1>Hello GitHub Pages</h1>
+    <>
+      <Navbar />
+
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+
+        <div style={{ padding: "30px" }}>
+          <h1>Dashboard</h1>
+          <p>Welcome to Habit Tracker</p>
+        </div>
+      </div>
+    </>
   );
 }
 
